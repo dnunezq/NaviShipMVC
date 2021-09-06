2914b1daff7637d250a8c676b7ae7c6546596e64
dnunezq <51251407+dnunezq@users.noreply.github.com>
2021-09-06 16:36:00 -0500

    Add files via upload

---
+++ source/NaviShipMVC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navishipmvc;
+
+import view.MainWind;
+
+/**
+ *
+ * @author david
+ */
+public class NaviShipMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainWind();
+    }
+    
+}
